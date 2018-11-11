e9c9f5cc53a83d335ca8d923d5dfdf591cb881ad
foryourselfand <foryourselfand@gmail.com>
2018-11-11 14:56:31 +0300

    Iterator

---
+++ src/L17_Iterator/Iterators/SongIterator.java
@@ -0,0 +1,7 @@
+package L17_Iterator.Iterators;
+
+import java.util.Iterator;
+
+public interface SongIterator {
+    public Iterator createIterator();
+}
---
+++ src/L17_Iterator/RadioStation.java
@@ -0,0 +1,19 @@
+package L17_Iterator;
+
+import L17_Iterator.Iterators.SongsOfThe70s;
+import L17_Iterator.Iterators.SongsOfThe80s;
+import L17_Iterator.Iterators.SongsOfThe90s;
+
+public class RadioStation {
+    public static void main(String[] args) {
+        SongsOfThe70s songs70s = new SongsOfThe70s();
+        SongsOfThe80s songs80s = new SongsOfThe80s();
+        SongsOfThe90s songs90s = new SongsOfThe90s();
+
+        DiscJockey madMike = new DiscJockey(songs70s, songs80s, songs90s);
+
+        // madMike.showTheSongs();
+
+        madMike.showTheSongs2();
+    }
+}
